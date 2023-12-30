e15044a6e6f4b1393039d7e49890ceccc50e6169
Ajay DAmerakunta <ajaydamerakunta8589@gmail.com>
2023-12-30 16:06:25 +0530

    changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,24 +1,43 @@
-
-
-export default function AboutMe() {
+import { PiStudentBold } from "react-icons/pi";
+const  AboutMe=()=> {
 
 
     return (
       <section id="AboutMe" className="about--section">
-        <div className="about--section--img">
-          <img src="img3-aj.jpg" alt="About Me" />
-        </div>
-        <div className="hero--section--content--box about--section--box">
-          <div className="hero--section--content">
+        <div className="about-section-img">
+          <div className="img-container">
+           <p>Get To Know More</p>
+           <h2>About Me</h2>
+           <img src="img3-aj.jpg" alt="About Me" />
+          </div>
+        
+
+        <div className="AboutMe-card-container">
+
+          <div className ="tech-container">
+          <PiStudentBold  className="edu-icon"/>
+            <h3>Fresher</h3>
+            <p>MERN Stack Development</p>
+          </div>
+
+          <div className="Education-container">
+             <PiStudentBold  className="edu-icon"/>
+            <h3 className="Education-head">Education</h3>
+            <p>B.Tech.Bachelors of Technology</p>
+          </div>
+          </div>
+
+          {/* <div className="hero--section--content"> */}
             {/* <p className="section--title"></p> */}
-            <h1 className="skills-section--heading">About Me</h1>
+            {/* <h1 className="skills-section--heading">About Me</h1> */}
             <p className="hero--section-description">
-             My self Ajay Damerakunta .Have completed B.Tech graduatio  in the year of 2022 in the stream of civil Engineering from Kakatiya University college of Engineering and Technology(KUCET). I  was a ex-intern  at ssds(smart squad digital solutions), which is a startup company. I have enhanced my skills as full stack developer, which I came to enhanced Html, Css, Javascript, React, Node js, Mongodb. 
+            Motivated B.Tech graduate with expertise in both front-end and back-end web development. Seeking a dynamic role to apply technical skills and creative problem-solving for innovative web solutions
             </p> 
             
 
-              </div>
+              {/* </div> */}
         </div>
       </section>
     );
   }
+  export default AboutMe
--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,157 +1,57 @@
+
+import React from 'react';
+import { MdVerified } from "react-icons/md";
+
 // import data from "../../data/index.json";
 
-// export default function MySkills() {
-//   return (
-//     <section className="skills--section" id="mySkills">
-//       <div className="portfolio--container">
-//         <p className="section--title">My Skills</p>
-//         <h2 className="skills--section--heading">My Expertise</h2>
-//       </div>
-//       <div className="skills--section--container">
-//         {data?.skills?.map((item, index) => (
-//           <div key={index} className="skills--section--card">
-//             <div className="skills--section--img">
-//               <img src={item.src} alt="Product Chain" />
-//             </div>
-//             <div className="skills--section--card--content">
-//               <h3 className="skills--section--title">{item.title}</h3>
-//               <p className="skills--section--description">{item.description}</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-import React from 'react';
-import data from "../../data/index.json";
-
-export default function MySkills() {
+const  MySkills =()=>{
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
-        <p className="section--title">My Skills</p>
-        <h2 className="skills--section--heading">My Expertise</h2>
-      </div>
-      <div className="skills--section--container">
-        {/* Render Frontend Skills */}
+        <p className="section-title">Explore My</p>
+        <h2 className="skills-heading">Experience</h2>
+      {/* </div> */}
+      <div className="Frontend-container">
+        <h1>Frontend Development</h1>
+        <div className="Frontend-card-container">
+        <MdVerified  className="verify-icon"/>
+          <h3>HTML</h3>
+          <p className="para">Intermediate</p>
+        </div>
+      
+
+        <div className="Frontend-card-container">
+        <MdVerified  className="verify-icon" />
+          <h3>CSS</h3>
+          <p>Intermediate</p>
+        </div>
+      
+
+       
+
+        <div className="Frontend-card-container">
+        <MdVerified />
+          <h3>JavaScript</h3>
+          <p>Intermediate</p>
+        </div>
+      
+        <div className="Frontend-card-container">
+        <MdVerified />
+          <h3>React Js</h3>
+          <p>Intermediate</p>
+        </div>
+      
         
+         
+      
         
-        <div className="skills--section--card">
-          <div className="front-end-container">
-        {/* {/* <h1 className="skills--section--title">Frontend</h1> */}
-        <button type="submit" className="skills--section--title">FrontEnd</button> 
-        </div>
-        <div className="tech--cards">
-
-          <div className="skills--section--img">
-            
-            <img src="./html-icon.png" alt="Frontend Icon"  className="react-icon" />
-            <p className="technology-name">HTML</p>
-          </div>
-
-          <div className="skills--section--img">
-            <img src="./css-icon.png" alt="Frontend Icon" className="react-icon" />
-            <p className="technology-name" >CSS</p>
-          </div>
-
-          <div className="skills--section--img">
-            <img src="./js-icon.png" alt="Frontend Icon"  className="react-icon" />
-            <p className="technology-name" >JAVASCRIPT</p>
-          </div>
-
-          <div className="skills--section--img">
-            <img src="./logo512.png" alt="Frontend Icon"  className="react-icon"/>
-            <p className="technology-name" >REACT</p>
-          </div>
-           {/* <div className="skills--section--img">
-            <img src="./logo512.png" alt="Frontend Icon" className="react-icon" />
-            <p className="technology-name">FIGMA</p>
-          </div>  */}
-
-          <div className="skills--section--card--content">
-           
-            {data.skills
-              .filter(skill => skill.title === "Front-end")
-              .map((item, index) => (
-                <div key={index} className="skills--section--description">
-                  {item.title}
-                </div>
-              ))}
-          </div>
         </div>
         </div>
-
-        {/* Render Backend Skills */}
-        <div className="skills--section--card">
-        <div className="front-end-container">
-        {/* <h1 className="skills--section--title">Backend</h1> */}
-        <button type="submit" className="skills--section--title">BackEnd</button> 
-        </div>
-
-        <div className="tech--cards">
-
-          <div className="skills--section--img">
-            <img src="./node-icon.png" alt="Backend Icon"  className="react-icon2"/>
-            <p className="technology-name" >NODE JS</p>
-          </div>
-
-           <div className="skills--section--img">
-            <img src="./express-icon.jpeg" alt="Backend Icon" className="react-icon2"/>
-            <p className="technology-name" >EXPRESS JS</p>
-          </div>
-          
-          
-
-
-          <div className="skills--section--card--content">
-        
-            {data.skills
-              .filter(skill => skill.title === "Back-end")
-              .map((item, index) => (
-                <div key={index} className="skills--section--description">
-                  {item.title}
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-
-
-      <div className="skills--section--card">
-        <div className="front-end-container">
-        {/* <h1 className="skills--section--title">Database</h1> */}
-        <button type="submit" className="skills--section--title">DataBase</button> 
-        </div>
-
-        <div className="tech--cards">
-
-          <div className="skills--section--img">
-            <img src="./db-icon.png" alt="Backend Icon"  className="react-icon3"/>
-            <p className="technology-name">Mongo DB</p>
-          </div>
-
-          <div className="skills--section--img">
-            <img src="./logo512.png" alt="Backend Icon" className="react-icon" />
-            <p className="technology-name">SQL</p>
-          </div>
-
-
-          <div className="skills--section--card--content">
-        
-            {data.skills
-              .filter(skill => skill.title === "Back-end")
-              .map((item, index) => (
-                <div key={index} className="skills--section--description">
-                  {item.title}
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-      </div>
+     
     </section>
   );
 }
+
+export default MySkills
 
 
